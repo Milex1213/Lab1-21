74868fd999f36a61471866606eaec86e8df2de34
Milex1213 <serge.alekseew2021@yandex.ru>
2026-04-28 13:22:56 +0400

    Update 2.py

--- 2 лаба/2.py
+++ 2 лаба/2.py
@@ -1,4 +1,3 @@
-```python
 import re
 
 with open("1.txt") as f:
@@ -15,13 +14,19 @@ i=0
 while i<len(a):
     if i+1<len(a):
         x,y=a[i],a[i+1]
-        if not f_flag or p%2==0:
+
+        if not f_flag:
             r.extend([y,x])
         else:
-            r.extend([x,y])
+            if p%2==0:
+                r.extend([y,x])
+            else:
+                r.extend([x,y])
             p+=1
+
         if re.search(r'000',x) or re.search(r'000',y):
             f_flag=True
+
         i+=2
     else:
         r.append(a[i])
@@ -32,4 +37,3 @@ if r:
     d={'0':'ноль','1':'один','2':'два','3':'три','4':'четыре',
        '5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}
     print(' '.join(d[c] for c in r[-1]))
-```
