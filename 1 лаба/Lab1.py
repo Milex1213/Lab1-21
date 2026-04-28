e446c52ccd79691ec96e654b541bdccc4d248ed3
Milex1213 <serge.alekseew2021@yandex.ru>
2026-04-28 13:14:35 +0400

    Update Lab1.py

--- 1 лаба/Lab1.py
+++ 1 лаба/Lab1.py
@@ -1,24 +1,33 @@
-d={'0':'ноль','1':'один','2':'два','3':'три','4':'четыре','5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}
-def s(n,st):
- n=list(n)
- for i in range(0 if st==2 else 2,len(n)-1,st):n[i],n[i+1]=n[i+1],n[i]
- return ''.join(n)
-def p(t):
- if not(t.isdigit() and int(t)%2 and len(t)>5):return
- c=t;v=set()
- while '000' not in c:
-  if c in v:break
-  v.add(c);c=s(c,2)
- return s(c,4)
-b='';r=[]
-for ch in open('1.txt').read():
- b+=ch
- if ch==' ':
-  z=p(b.strip())
-  if z:r.append(z)
-  b=''
-if b.strip():z=p(b.strip())
-if z and z not in r:r.append(z)
+with open("input.txt") as f:
+    a = [x for x in f.read().split() if len(x) > 5 and int(x) % 2 == 1]
+
+r = []
+f = False
+p = 0
+i = 0
+
+while i < len(a):
+    if i + 1 < len(a):
+        x, y = a[i], a[i+1]
+
+        if not f or p % 2 == 0:
+            r += [y, x]
+        else:
+            r += [x, y]
+            p += 1
+
+        if '000' in x or '000' in y:
+            f = True
+
+        i += 2
+    else:
+        r.append(a[i])
+        i += 1
+
+for x in r[:-1]:
+    print(x, end=' ')
+
 if r:
- [print(i,end=' ') for i in r[:-1]]
- print(' '.join(d[j] for j in r[-1]))
+    d = {'0':'ноль','1':'один','2':'два','3':'три','4':'четыре',
+         '5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}
+    print(' '.join(d[c] for c in r[-1]))
