73cb6ce6a6676f99f1d38207bd30aa1c4754b6a8
Milex1213 <serge.alekseew2021@yandex.ru>
2026-04-29 10:17:13 +0400

    Update 3labaWORK.py

--- 3 лаба/3labaWORK.py
+++ 3 лаба/3labaWORK.py
@@ -1,83 +1,86 @@
-from math import prod
+import numpy as np
 
-# Чтение матрицы из файла
-def read_matrix(filename):
-    return [list(map(int, line.split())) for line in open(filename)]
-
-# Вывод матрицы
-def print_matrix(m, name):
+def print_matrix(name, M):
     print(f"\n{name}:")
-    for row in m:
+    for row in M:
         print(" ".join(f"{x:4}" for x in row))
 
-#Транспонирование матрицы
-def transpose(m):
-    return [list(row) for row in zip(*m)]
 
-#Получение индексов для четырех областей матрицы
-def get_diagonal_regions(n):
-    a1, a2, a3, a4 = [], [], [], []
-    for i in range(n):
-        for j in range(n):
-            if i < j and i + j < n - 1: a1.append((i, j))#Области от1-4
-            elif i < j and i + j > n - 1: a2.append((i, j))  
-            elif i > j and i + j > n - 1: a3.append((i, j))  
-            elif i > j and i + j < n - 1: a4.append((i, j))
-    return a1, a2, a3, a4
+def split_matrix(A):
+    n = A.shape[0]
+    mid = n // 2
+    
+    area1 = A[:mid, :mid]
+    area2 = A[:mid, mid:]
+    area3 = A[mid:, :mid]
+    area4 = A[mid:, mid:]
+    
+    return area1.copy(), area2.copy(), area3.copy(), area4.copy()
 
-# Тут идёт построение матрицы F
-def build_F(A, K):
-    n = len(A)
-    F = [row[:] for row in A]  # Копируем матрицу A в F
-    a1, a2, a3, _ = get_diagonal_regions(n)
 
-    # Подсчет количества чисел, больших K, в четных столбцах области 1
-    count_greater_K = sum(1 for i, j in a1 if j % 2 == 0 and A[i][j] > K)
+def count_condition(area2, area4):
+    count_pos = 0
+    for j in range(area2.shape[1]):
+        if j % 2 == 1:
+            count_pos += np.sum(area2[:, j] > 0)
 
-    # Сумма чисел в нечетных строках области 3
-    sum_odd_rows = sum(A[i][j] for i, j in a3 if i % 2 == 1)
+    count_neg = 0
+    for j in range(area4.shape[1]):
+        if j % 2 == 0:
+            count_neg += np.sum(area4[:, j] < 0)
 
-     # Проверяется условия
-    if count_greater_K > sum_odd_rows:
-        print("\nУсловие выполнено: количество чисел, больших K, в четных столбцах области 1 больше суммы чисел в нечетных строках области 3.")
-        print("Выполняется симметричная замена областей 1 и 3...")
-        for (i1, j1), (i3, j3) in zip(a1, a3[::-1]):
-            F[i1][j1], F[i3][j3] = F[i3][j3], F[i1][j1]
+    return count_pos, count_neg
+
+
+def build_F(A):
+    n = A.shape[0]
+    F = A.copy()
+    
+    a1, a2, a3, a4 = split_matrix(A)
+    
+    pos, neg = count_condition(a2, a4)
+    
+    print(f"\nПоложительных в области 2 (четные столбцы): {pos}")
+    print(f"Отрицательных в области 4 (нечетные столбцы): {neg}")
+    
+    mid = n // 2
+
+    if pos > neg:
+        F[:mid, :mid] = np.fliplr(a2)
+        F[:mid, mid:] = np.fliplr(a1)
     else:
-        print("\nУсловие не выполнено: количество чисел, больших K, в четных столбцах области 1 меньше или равно сумме чисел в нечетных строках области 3.")
-        print("Выполняется несимметричная замена областей 2 и 3...")
-        for (i2, j2), (i3, j3) in zip(a2, a3):
-            F[i2][j2], F[i3][j3] = F[i3][j3], F[i2][j2]
-    return F, count_greater_K, sum_odd_rows
+        F[mid:, :mid] = a4
+        F[mid:, mid:] = a3
 
-# Вычисление выражения A * F - K * A^T
-def compute_result(A, F, K):
-    n = len(A)
-    A_T = transpose(A)  # Транспонированная матрица A
-    F_T = transpose(F)  # Транспонированная матрица F
+    return F
 
-    # Умножение матриц A и F
-    AF = [[sum(A[i][k] * F[k][j] for k in range(n)) for j in range(n)] for i in range(n)]
 
-    # Умножение матрицы A^T на K
-    KAT = [[K * A_T[i][j] for j in range(n)] for i in range(n)]
+def main():
+    K = int(input("Введите K: "))
+    N = int(input("Введите N: "))
 
-    # Вычитание матриц
-    result = [[AF[i][j] - KAT[i][j] for j in range(n)] for i in range(n)]
+    print("Введите матрицу A построчно:")
+    A = []
+    for _ in range(N):
+        row = list(map(int, input().split()))
+        A.append(row)
 
-    return result
+    A = np.array(A)
 
-K = int(input("Введите K: "))
-A = read_matrix("matrix.txt")
+    print_matrix("Матрица A", A)
 
-# Формирование матрицы F
-F, count_greater_K, sum_odd_rows = build_F(A, K)
+    F = build_F(A)
+    print_matrix("Матрица F", F)
 
-# Вычисление результата выражения
-R = compute_result(A, F, K)
+    AT = A.T
+    print_matrix("A^T", AT)
 
-print_matrix(A, "Исходная матрица A")
-print(f"\nКоличество чисел, больших K, в четных столбцах области 1: {count_greater_K}")
-print(f"Сумма чисел в нечетных строках области 3: {sum_odd_rows}")
-print_matrix(F, "Матрица F после замены")
-print_matrix(R, "Результат выражения A * F - K * A^T")
+    FA = F + A
+    print_matrix("F + A", FA)
+
+    result = np.dot(FA, AT) - K * F
+    print_matrix("(F + A) * A^T - K * F", result)
+
+
+if __name__ == "__main__":
+    main()
