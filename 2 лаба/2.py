28c4633143717847c086a8a68090c60769eecc63
Milex1213 <serge.alekseew2021@yandex.ru>
2026-04-29 09:00:07 +0400

    Update 2.py

--- 2 лаба/2.py
+++ 2 лаба/2.py
@@ -3,37 +3,34 @@ import re
 with open("1.txt") as f:
     text=f.read()
 
-nums=re.findall(r'\d+',text)
-a=[x for x in nums if len(x)>5 and int(x)%2==1]
+nums=re.findall(r'\b\d{5,}[13579]\b',text)
 
 r=[]
-f_flag=False
-p=0
+found=False
+pair_index=0
 i=0
 
-while i<len(a):
-    if i+1<len(a):
-        x,y=a[i],a[i+1]
-
-        if not f_flag:
-            r.extend([y,x])
+while i<len(nums):
+    if i+1<len(nums):
+        x,y=nums[i],nums[i+1]
+        if not found:
+            r+=[y,x]
+            if re.search(r'000',x) or re.search(r'000',y):
+                found=True
         else:
-            if p%2==0:
-                r.extend([y,x])
+            if pair_index%2==1:
+                r+=[y,x]
             else:
-                r.extend([x,y])
-            p+=1
-
-        if re.search(r'000',x) or re.search(r'000',y):
-            f_flag=True
-
+                r+=[x,y]
+            pair_index+=1
         i+=2
     else:
-        r.append(a[i])
+        r.append(nums[i])
         i+=1
 
+for x in r[:-1]:
+    print(x,end=' ')
+
 if r:
-    print(' '.join(r[:-1]),end=' ')
-    d={'0':'ноль','1':'один','2':'два','3':'три','4':'четыре',
-       '5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}
+    d={'0':'ноль','1':'один','2':'два','3':'три','4':'четыре','5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}
     print(' '.join(d[c] for c in r[-1]))
