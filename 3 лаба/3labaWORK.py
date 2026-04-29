d12386ec3a6467b8a4bda4e69df1f05facce29e5
Milex1213 <serge.alekseew2021@yandex.ru>
2026-04-29 11:01:02 +0400

    Update 3labaWORK.py

--- 3 лаба/3labaWORK.py
+++ 3 лаба/3labaWORK.py
@@ -1,20 +1,18 @@
-import numpy as np
-
-with open("input.txt", "r") as f:
+with open("input.txt", "r", encoding="utf-8-sig") as f:
     A = [list(map(int, line.split())) for line in f if line.strip()]
 
-A = np.array(A)
-N = A.shape[0]
+N = len(A)
 
-if A.shape[0] != A.shape[1]:
+if any(len(row) != N for row in A):
     raise ValueError("Матрица должна быть квадратной!")
 
 K = int(input("Введите K: "))
 
 print("Матрица A:")
-print(A)
+for row in A:
+    print(row)
 
-F = A.copy()
+F = [row[:] for row in A]
 
 mid = N // 2
 
@@ -29,44 +27,93 @@ else:
     c1, c2 = 0, mid
     c3, c4 = mid + 1, N
 
-area1 = F[r1:r2, c1:c2].copy()
-area2 = F[r1:r2, c3:c4].copy()
-area3 = F[r3:r4, c1:c2].copy()
-area4 = F[r3:r4, c3:c4].copy()
+def get_area(M, r1, r2, c1, c2):
+    return [row[c1:c2] for row in M[r1:r2]]
+
+area1 = get_area(F, r1, r2, c1, c2)
+area2 = get_area(F, r1, r2, c3, c4)
+area3 = get_area(F, r3, r4, c1, c2)
+area4 = get_area(F, r3, r4, c3, c4)
 
 count_pos_2 = 0
-for i in range(area2.shape[0]):
-    for j in range(area2.shape[1]):
-        if j % 2 == 1 and area2[i, j] > 0:
+for i in range(len(area2)):
+    for j in range(len(area2[0])):
+        if j % 2 == 1 and area2[i][j] > 0:
             count_pos_2 += 1
 
 count_neg_4 = 0
-for i in range(area4.shape[0]):
-    for j in range(area4.shape[1]):
-        if j % 2 == 0 and area4[i, j] < 0:
+for i in range(len(area4)):
+    for j in range(len(area4[0])):
+        if j % 2 == 0 and area4[i][j] < 0:
             count_neg_4 += 1
 
 print("Положительных (обл 2):", count_pos_2)
 print("Отрицательных (обл 4):", count_neg_4)
 
+def flip_lr(matrix):
+    return [row[::-1] for row in matrix]
+
+def insert_area(M, area, r1, c1):
+    for i in range(len(area)):
+        for j in range(len(area[0])):
+            M[r1 + i][c1 + j] = area[i][j]
+
 if count_pos_2 > count_neg_4:
     print("Меняем области 1 и 2 симметрично")
-    F[r1:r2, c1:c2] = np.fliplr(area2)
-    F[r1:r2, c3:c4] = np.fliplr(area1)
+    insert_area(F, flip_lr(area2), r1, c1)
+    insert_area(F, flip_lr(area1), r1, c3)
 else:
     print("Меняем области 3 и 4 несимметрично")
-    F[r3:r4, c1:c2] = area4
-    F[r3:r4, c3:c4] = area3
+    insert_area(F, area4, r3, c1)
+    insert_area(F, area3, r3, c3)
 
 print("Матрица F:")
-print(F)
+for row in F:
+    print(row)
 
-AT = A.T
+def transpose(M):
+    return [list(row) for row in zip(*M)]
+
+AT = transpose(A)
 
 print("A^T:")
-print(AT)
+for row in AT:
+    print(row)
 
-result = (F + A) @ AT - K * F
+def add_matrix(A, B):
+    return [[A[i][j] + B[i][j] for j in range(len(A))] for i in range(len(A))]
 
-print("Результат (F + A) * A^T - K * F:")
-print(result)
+def multiply(A, B):
+    N = len(A)
+    result = [[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+def mul_scalar(M, k):
+    return [[M[i][j] * k for j in range(len(M))] for i in range(len(M))]
+
+def subtract(A, B):
+    return [[A[i][j] - B[i][j] for j in range(len(A))] for i in range(len(A))]
+
+sum_FA = add_matrix(F, A)
+print("(F + A):")
+for row in sum_FA:
+    print(row)
+
+mult = multiply(sum_FA, AT)
+print("(F + A) * A^T:")
+for row in mult:
+    print(row)
+
+KF = mul_scalar(F, K)
+print("K * F:")
+for row in KF:
+    print(row)
+
+result = subtract(mult, KF)
+print("Результат:")
+for row in result:
+    print(row)
