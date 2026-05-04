aa339535f8dad4821870d5910f812a22abf3b8f0
Milex1213 <serge.alekseew2021@yandex.ru>
2026-05-04 12:27:18 +0400

    Update 3labaWORK.py

--- 3 лаба/3labaWORK.py
+++ 3 лаба/3labaWORK.py
@@ -15,10 +15,14 @@ F = A.copy()
 
 mid = N // 2
 
+# 1 — левая
 area1 = (slice(None), slice(0, mid))
+# 2 — верхняя
 area2 = (slice(0, mid), slice(None))
+# 3 — правая
 area3 = (slice(None), slice(mid, N))
-area41 = (slice(mid, N), slice(None))
+# 4 — нижняя
+area4 = (slice(mid, N), slice(None))
 
 count_pos = 0
 for i in range(0, mid):
@@ -36,13 +40,15 @@ print("\nПоложительных:", count_pos)
 print("Отрицательных:", count_neg)
 
 if count_pos > count_neg:
+    print("\nСимметричный обмен областей 1 и 2")
     temp = F[area1].copy()
     F[area1] = F[area2].T
     F[area2] = temp.T
 else:
+    print("\nНесимметричный обмен областей 3 и 4")
     temp = F[area3].copy()
-    F[area3] = F[area41]
-    F[area41] = temp
+    F[area3] = F[area4]
+    F[area4] = temp
 
 print("\nМатрица F:")
 print(F)
