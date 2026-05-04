07d726c1b7b9d0d2b9397f6ed36c52e94a2720b9
Milex1213 <serge.alekseew2021@yandex.ru>
2026-05-04 12:17:43 +0400

    Update 3labaWORK.py

--- 3 лаба/3labaWORK.py
+++ 3 лаба/3labaWORK.py
@@ -1,119 +1,68 @@
-with open("input.txt", "r", encoding="utf-8-sig") as f:
-    A = [list(map(int, line.split())) for line in f if line.strip()]
-
-N = len(A)
-
-if any(len(row) != N for row in A):
-    raise ValueError("Матрица должна быть квадратной!")
+import numpy as np
 
 K = int(input("Введите K: "))
 
-print("Матрица A:")
-for row in A:
-    print(row)
+with open("matrix.txt", "r") as f:
+    N = int(f.readline())
+    A = [list(map(int, f.readline().split())) for _ in range(N)]
 
-F = [row[:] for row in A]
+A = np.array(A)
+
+print("\nМатрица A:")
+print(A)
+
+F = A.copy()
 
 mid = N // 2
 
-if N % 2 == 0:
-    r1, r2 = 0, mid
-    r3, r4 = mid, N
-    c1, c2 = 0, mid
-    c3, c4 = mid, N
+area1 = (slice(None), slice(0, mid))
+area2 = (slice(0, mid), slice(None))
+area3 = (slice(None), slice(mid, N))
+area41 = (slice(mid, N), slice(None))
+
+count_pos = 0
+for i in range(0, mid):
+    for j in range(N):
+        if j % 2 == 0 and A[i][j] > 0:
+            count_pos += 1
+
+count_neg = 0
+for i in range(mid, N):
+    for j in range(N):
+        if j % 2 != 0 and A[i][j] < 0:
+            count_neg += 1
+
+print("\nПоложительных:", count_pos)
+print("Отрицательных:", count_neg)
+
+if count_pos > count_neg:
+    temp = F[area1].copy()
+    F[area1] = F[area2].T
+    F[area2] = temp.T
 else:
-    r1, r2 = 0, mid
-    r3, r4 = mid + 1, N
-    c1, c2 = 0, mid
-    c3, c4 = mid + 1, N
+    temp = F[area3].copy()
+    F[area3] = F[area41]
+    F[area41] = temp
 
-def get_area(M, r1, r2, c1, c2):
-    return [row[c1:c2] for row in M[r1:r2]]
+print("\nМатрица F:")
+print(F)
 
-area1 = get_area(F, r1, r2, c1, c2)
-area2 = get_area(F, r1, r2, c3, c4)
-area3 = get_area(F, r3, r4, c1, c2)
-area4 = get_area(F, r3, r4, c3, c4)
+sum_FA = F + A
+print("\nF + A:")
+print(sum_FA)
 
-count_pos_2 = 0
-for i in range(len(area2)):
-    for j in range(len(area2[0])):
-        if j % 2 == 1 and area2[i][j] > 0:
-            count_pos_2 += 1
+AT = A.T
+print("\nA^T:")
+print(AT)
 
-count_neg_4 = 0
-for i in range(len(area4)):
-    for j in range(len(area4[0])):
-        if j % 2 == 0 and area4[i][j] < 0:
-            count_neg_4 += 1
+mult = np.dot(sum_FA, AT)
+print("\n(F + A) * A^T:")
+print(mult)
 
-print("Положительных (обл 2):", count_pos_2)
-print("Отрицательных (обл 4):", count_neg_4)
+KF = K * F
+print("\nK * F:")
+print(KF)
 
-def flip_lr(matrix):
-    return [row[::-1] for row in matrix]
-
-def insert_area(M, area, r1, c1):
-    for i in range(len(area)):
-        for j in range(len(area[0])):
-            M[r1 + i][c1 + j] = area[i][j]
-
-if count_pos_2 > count_neg_4:
-    print("Меняем области 1 и 2 симметрично")
-    insert_area(F, flip_lr(area2), r1, c1)
-    insert_area(F, flip_lr(area1), r1, c3)
-else:
-    print("Меняем области 3 и 4 несимметрично")
-    insert_area(F, area4, r3, c1)
-    insert_area(F, area3, r3, c3)
-
-print("Матрица F:")
-for row in F:
-    print(row)
-
-def transpose(M):
-    return [list(row) for row in zip(*M)]
-
-AT = transpose(A)
-
-print("A^T:")
-for row in AT:
-    print(row)
-
-def add_matrix(A, B):
-    return [[A[i][j] + B[i][j] for j in range(len(A))] for i in range(len(A))]
-
-def multiply(A, B):
-    N = len(A)
-    result = [[0]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            for k in range(N):
-                result[i][j] += A[i][k] * B[k][j]
-    return result
-
-def mul_scalar(M, k):
-    return [[M[i][j] * k for j in range(len(M))] for i in range(len(M))]
-
-def subtract(A, B):
-    return [[A[i][j] - B[i][j] for j in range(len(A))] for i in range(len(A))]
-
-sum_FA = add_matrix(F, A)
-print("(F + A):")
-for row in sum_FA:
-    print(row)
-
-mult = multiply(sum_FA, AT)
-print("(F + A) * A^T:")
-for row in mult:
-    print(row)
-
-KF = mul_scalar(F, K)
-print("K * F:")
-for row in KF:
-    print(row)
-
-result = subtract(mult, KF)
-print("Результат:")
-for row in result:
-    print(row)
+result = mult - KF
+print("\nРезультат:")
+print(result)
